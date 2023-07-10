1574f952e1fb3b2216492f15cdcbb4c942439b3c
RufioYrael <4.3.1e.1987@gmail.com>
2023-07-10 19:18:36 -0400

    i totally forgot everything.... lmao

---
+++ app.js
@@ -0,0 +1,23 @@
+//creating the javascript to handle increasing number in html and decreasing number in html
+let num = 0
+const increase = document.getElementById('#increase')
+const decrease = document.getElementById('#decrease')
+
+
+
+increase.addEventListener('click', function () {
+    displayNum += userInput
+    document.querySelector('#number').textContent = number
+    document.querySelector('#counter').textContent = displayNum
+})
+
+decrease.addEventListener('click', function () {
+    displayNum -= userInput
+    document.querySelector('#number').textContent = number
+    //if number is less than 0, change the color to red
+    if (number < 0) {
+        document.querySelector('#number').style.color = 'red'
+    }
+    document.querySelector('#counter').textContent = displayNum
+})
+
